2c04ae395e25c697eb5231687516c11ea40ab689
zyf <zhaoyafei0210@gmail.com>
2018-06-27 01:40:49 +0800

    add scripts/*

---
+++ scripts/face_aligner/batch_crop_112x112_nodet/_init_paths.py
@@ -0,0 +1,13 @@
+import os.path as osp
+import sys
+
+
+def add_path(path):
+    if path not in sys.path:
+        sys.path.insert(0, path)
+
+
+add_path(osp.abspath('../../../'))
+
+# add mxnet path
+# add_path('/opt/mxnet')
